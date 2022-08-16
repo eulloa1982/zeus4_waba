4ffb9d701d3e6efcebb1f27ef4e8cef9f545fbeb
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2022-08-16 19:57:19 -0400

    Cambiando iconos

--- client/src/components/WriteFromPrevMsgs/WriteFromPrevMsgs.js
+++ client/src/components/WriteFromPrevMsgs/WriteFromPrevMsgs.js
@@ -24,7 +24,7 @@ class WriteFromPrevMsgs extends React.Component {
                 ) 
                 : 
                 (
-                  <span class="message-status"><img src="./images/status.svg" alt="attach" /></span>
+                  <span class="message-status"><img src="./images/error.png" alt="attach" /></span>
                 )}                  
             </div>
             
