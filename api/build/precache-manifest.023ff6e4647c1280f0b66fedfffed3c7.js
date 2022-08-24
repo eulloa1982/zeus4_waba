3b0d87f14b59fa9ab00babec206f893a29e06fb7
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2022-08-24 17:03:31 -0400

    Subiendo cambios a produccion
    Refactorizacion de componentes de clases a funcionales

---
+++ api/build/precache-manifest.023ff6e4647c1280f0b66fedfffed3c7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e0f41aca0b40f542301",
+    "url": "/static/css/main.0fefb970.chunk.css"
+  },
+  {
+    "revision": "3e0f41aca0b40f542301",
+    "url": "/static/js/main.3e0f41ac.chunk.js"
+  },
+  {
+    "revision": "6336b426631f80f6c7a2",
+    "url": "/static/js/1.6336b426.chunk.js"
+  },
+  {
+    "revision": "f537ee759dd18cc2a267",
+    "url": "/static/css/2.c1903f96.chunk.css"
+  },
+  {
+    "revision": "f537ee759dd18cc2a267",
+    "url": "/static/js/2.f537ee75.chunk.js"
+  },
+  {
+    "revision": "0979ea937a1ee6514631",
+    "url": "/static/js/runtime~main.0979ea93.js"
+  },
+  {
+    "revision": "423f89706510a5394bd905ff550fc4d4",
+    "url": "/static/media/bg-chat.423f8970.png"
+  },
+  {
+    "revision": "951017bb35fdbeea5d34bbc2fa9b5a45",
+    "url": "/static/media/Loading.951017bb.gif"
+  },
+  {
+    "revision": "7b8bc5d59e84273659876103ae09970d",
+    "url": "/index.html"
+  }
+];
--- api/build/service-worker.js
+++ api/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.27274ccbc73b2eb283a9fed22fb27fbb.js"
+  "/precache-manifest.023ff6e4647c1280f0b66fedfffed3c7.js"
 );
 
 workbox.clientsClaim();
